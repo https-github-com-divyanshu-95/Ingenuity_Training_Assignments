60aee44f9a87f1144a0aac7aca5cc85632d79f37
Divyanshu Varshney <51895957+divyanshu-95@users.noreply.github.com>
2021-12-10 23:36:24 +0530

    Add files via upload

--- webpack.config.js
+++ webpack.config.js
@@ -1,78 +1,138 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-const CopyPlugin = require("copy-webpack-plugin");
+const webpack = require('webpack');
+const path = require('path');
+const pkg = require('./package.json');
 
-const isProduction = process.env.NODE_ENV == "production";
+// variables
+const isProduction = process.argv.indexOf('-p') >= 0 || process.env.NODE_ENV === 'production';
+const sourcePath = path.join(__dirname, './src');
+const outPath = path.join(__dirname, './build');
 
-const stylesHandler = MiniCssExtractPlugin.loader;
+// plugins
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
-const config = {
-  entry: "./src/index.ts",
+module.exports = {
+  context: sourcePath,
+  entry: {
+    app: './index.ts'
+  },
   output: {
-    path: path.resolve(__dirname, "dist"),
+    path: outPath,
+    filename: isProduction ? '[contenthash].js' : '[hash].js',
+    chunkFilename: isProduction ? '[name].[contenthash].js' : '[name].[hash].js'
   },
-  devServer: {
-    open: true,
-    host: "localhost",
-    port: 8080,
-    compress: true,
+  target: 'web',
+  resolve: {
+    extensions: ['.js', '.ts'],
+    // Fix webpack's default behavior to not load packages with jsnext:main module
+    // (jsnext:main directs not usually distributable es6 format, but es6 sources)
+    mainFields: ['module', 'browser', 'main'],
+    alias: {
+      app: path.resolve(__dirname, 'src/app/')
+    }
   },
-  plugins: [
-    new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({
-      template: "src/index.html",
-      inject: "body"
-    }),
-
-    new MiniCssExtractPlugin(),
-
-    new CopyPlugin({
-      patterns: [
-        { from: "src/assets", to: "assets" },
-      ],
-    }),
-
-    // Add your plugins here
-    // Learn more about plugins from https://webpack.js.org/configuration/plugins/
-  ],
   module: {
     rules: [
+      // .ts
       {
-        test: /\.(ts|tsx)$/i,
-        loader: "ts-loader",
-        exclude: ["/node_modules/"],
+        test: /\.ts$/,
+        use: ['ts-loader']
       },
+      // css
       {
-        test: /\.css$/i,
-        use: [stylesHandler, "css-loader", "postcss-loader"],
+        test: /\.css$/,
+        use: [
+          isProduction ? MiniCssExtractPlugin.loader : 'style-loader',
+          {
+            loader: 'css-loader',
+            query: {
+              sourceMap: !isProduction,
+              importLoaders: 1,
+              modules: {
+                localIdentName: isProduction ? '[hash:base64:5]' : '[local]__[hash:base64:5]'
+              }
+            }
+          }
+        ]
       },
+      // static assets
+      { test: /\.html$/, use: 'html-loader' },
       {
-        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
-        type: "asset",
-      },
-
-      // Add your rules for custom modules here
-      // Learn more about loaders from https://webpack.js.org/loaders/
-    ],
+        test: /\.(jpe?g|png|gif|bmp|mp3|mp4|ogg|wav|eot|ttf|woff|woff2)$/,
+        use: {
+          loader: 'file-loader',
+          options: {
+            outputPath: 'assets'
+          }
+        }
+      }
+    ]
   },
-  resolve: {
-    extensions: [".tsx", ".ts", ".js"],
+  optimization: {
+    splitChunks: {
+      name: true,
+      cacheGroups: {
+        commons: {
+          chunks: 'initial',
+          minChunks: 2
+        },
+        vendors: {
+          test: /[\\/]node_modules[\\/]/,
+          chunks: 'all',
+          filename: isProduction ? 'vendor.[contenthash].js' : 'vendor.[hash].js',
+          priority: -10
+        }
+      }
+    },
+    runtimeChunk: true
   },
-};
-
-module.exports = () => {
-  if (isProduction) {
-    config.mode = "production";
-    config.output.filename = "index.[fullhash].js";
-    config.optimization = {
-      minimize: true,
-    };
-  } else {
-    config.mode = "development";
-    config.devtool = "inline-source-map";
-    config.output.filename = "index.[hash].js";
+  plugins: [
+    new webpack.EnvironmentPlugin({
+      NODE_ENV: 'development', // use 'development' unless process.env.NODE_ENV is defined
+      DEBUG: false
+    }),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: '[hash].css',
+      disable: !isProduction
+    }),
+    new HtmlWebpackPlugin({
+      template: '../index.html',
+      minify: {
+        minifyJS: true,
+        minifyCSS: true,
+        removeComments: true,
+        useShortDoctype: true,
+        collapseWhitespace: true,
+        collapseInlineTagWhitespace: true
+      },
+      append: {
+        head: `<script src="https://cdn.polyfill.io/v3/polyfill.min.js"></script>`
+      },
+      meta: {
+        title: pkg.name,
+        description: pkg.description,
+        keywords: Array.isArray(pkg.keywords) ? pkg.keywords.join(',') : undefined
+      }
+    })
+  ],
+  devServer: {
+    contentBase: sourcePath,
+    hot: true,
+    inline: true,
+    historyApiFallback: {
+      disableDotRule: true
+    },
+    stats: 'minimal',
+    clientLogLevel: 'warning'
+  },
+  // https://webpack.js.org/configuration/devtool/
+  devtool: isProduction ? 'source-map' : 'cheap-module-source-map',
+  node: {
+    // workaround for webpack-dev-server issue
+    // https://github.com/webpack/webpack-dev-server/issues/60#issuecomment-103411179
+    fs: 'empty',
+    net: 'empty'
   }
-  return config;
 };
